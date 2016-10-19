f2ca5714fc1712ea6dee2117a625ed1b373fd65b
S-H-Y-GitHub <932495000@qq.com>
2016-10-19 16:29:09 +0800

    完成读取文件

--- KDTree/KBTree.cpp
+++ KDTree/KBTree.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 struct KDDData
 {
-	//基本属性集
+	/*//基本属性集
 	double duration;		//length (number of seconds) of the connection					0
 	string protocol_type;	//type of the protocol, e.g. tcp, udp, etc.						1
 	string service;			//network service on the destination, e.g., http, telnet,etc.	2
@@ -26,19 +26,61 @@ struct KDDData
 	double same_srv_rate;	//% of connections to the same service							28
 	double diff_srv_rate;	//% of connections to different services						29
 	double srv_diff_host_rate;//% of connections to different hosts							30
-	//忽略特征31~40
-	string label;			//这个连接的类型												41
+	//忽略特征31~40*/
+	double properties[9];	//所有的流量属性，对应源数据中的第22-30项
+	string label;			//这个连接的类型，back，land，neptune，pod，teardrop，smurf表示DoS攻击			41
 };
 
-void readData(const string filename, vector<KDDData> datas)
-{  //从KDD数据文件中读取数据
-	KDDData datarow;
-	datas.push_back(datarow);
+vector<string> split(string s, string p)
+{	//将s字符串按p分割
+	int pos;
+	int size = s.size();
+	vector<string> result;
+	s += p;
+	for (int i = 0; i < size; i++)
+	{
+		pos = s.find(p, i);
+		if (pos <= size)
+		{
+			string temp = s.substr(i, pos - i);
+			result.push_back(temp);
+			i = pos + p.size() - 1;
+		}
+	}
+	return result;
+}
 
+vector<KDDData> readData(const string filename)
+{	//从KDD数据文件中读取数据
+	vector<KDDData> result;
+	KDDData data;
+	vector<string> temp;
+	string s;
+	ifstream in;
+	in.open(filename);
+	if (in.fail())
+	{
+		cerr << "打开文件失败" << endl;
+		exit(-1);
+	}
+	while (getline(in, s))
+	{
+		temp = split(s, ",");
+		for (int i = 0; i < 9; i++)
+			data.properties[i] = atof(temp[i + 22].data());
+		data.label = temp[41];
+		result.push_back(data);
+	}
+	in.close();
+	return result;
 }
 
 int main()
 {
-
+	KDDData b;
+	vector<KDDData> a;
+	a.push_back(b);
+	b.count = 0;
+	a.push_back(b);
 	return 0;
 }
