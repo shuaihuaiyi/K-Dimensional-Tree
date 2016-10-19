b2af41c7b6a761e082ed3695edd6a03597f6a5f8
S-H-Y-GitHub <932495000@qq.com>
2016-10-19 13:37:11 +0800

    完成架构设计

--- KDTree/KBTree.cpp
+++ KDTree/KBTree.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <fstream>
 #include "KBTree.h"
-
 using namespace std;
+
+struct KDDData
+{
+	double duration;		//length (number of seconds) of the connection
+	string protocol_type;	//type of the protocol, e.g. tcp, udp, etc.
+	string service;			//network service on the destination, e.g., http, telnet,etc.
+	double src_bytes;		//number of data bytes from source to destination
+
+};
+
+void readData(const string filename, vector<KDDData> datas)
+{  //从KDD数据文件中读取数据
+	KDDData datarow;
+	datas.push_back(datarow);
+
+}
+
 int main()
 {
 
--- KDTree/KBTree.h
+++ KDTree/KBTree.h
@@ -1,1 +1,26 @@
 #pragma once
+#include <vector>
+#include <string>
+
+struct KDTreeSpliter
+{
+
+};
+
+class KDTree
+{
+public:
+	KDTree();
+	~KDTree();
+	
+private:
+
+};
+
+KDTree::KDTree()
+{
+}
+
+KDTree::~KDTree()
+{
+}
